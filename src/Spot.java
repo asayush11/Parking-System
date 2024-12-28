69607d3aea8384a94a61e088b4370c0c8545384d
Ayush Sharma <asayush35@gmail.com>
2024-12-28 15:19:29 +0530

    adding pricing feature

--- src/Spot.java
+++ src/Spot.java
@@ -1,11 +1,12 @@
 package src;
 
-//import Vehicle;
-//import parkinglot.vehicletype.VehicleType;
+import java.sql.Timestamp;
+
 public class Spot {
     private final int spotNumber;
     private final VehicleType vehicleType;
     private Vehicle parkedVehicle;
+    private Timestamp parkTime;
 
     public Spot(int spotNumber, VehicleType vehicleType) {
         this.spotNumber = spotNumber;
@@ -19,6 +20,7 @@ public class Spot {
     public synchronized void parkVehicle(Vehicle vehicle) {
         if (isAvailable() && vehicle.getType() == vehicleType) {
             parkedVehicle = vehicle;
+            parkTime = new Timestamp((System.currentTimeMillis()));
         } else {
             throw new IllegalArgumentException("Cannot Park.");
         }
@@ -26,6 +28,12 @@ public class Spot {
 
     public synchronized void unparkVehicle() {
         parkedVehicle = null;
+        long parkDuration = (new Timestamp((System.currentTimeMillis()))).getTime() - parkTime.getTime();
+        // it should be hours in real world
+        double durationInMilliSeconds = parkDuration / 1000.0;
+        double cost = (durationInMilliSeconds * 10.0);
+        System.out.println("Amount to be paid: " + cost);
+        parkTime = null;
     }
 
     public VehicleType getVehicleType() {
